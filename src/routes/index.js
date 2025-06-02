a615135e240d29c1c6dd2c13c398ed88bc1b2799
ali purnama alam <alipurnama69@gmail.com>
2025-06-02 16:31:45 +0700

    fix(delete uncomplete coa)

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const authRoutes = require("./auth.routes");
 const masterCoaRoutes = require("./master_coa.routes");
 const userRoutes = require("./user.routes");
+const roleRoutes = require("./role.routes");
 
 // Use routes
 router.use("/auth", authRoutes);
 router.use("/coa", masterCoaRoutes);
 router.use("/users", userRoutes);
+router.use("/role", roleRoutes);
 
 module.exports = router;
